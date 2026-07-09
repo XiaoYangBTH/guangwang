daba555fd44dd9cf5644fca7ecde0cb57eeef2ac
XiaoYangBTH <1351072690@qq.com>
2026-07-09 09:11:53 +0800

    1

--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="pp-container">
     <header class="pp-header">
-      <h1>Test1 Privacy Policy</h1>
+      <h1>Whooli Privacy Policy</h1>
       <p class="pp-updated">Date of the latest edition: May 16, 2022</p>
     </header>
 
     <p class="pp-intro">
       This policy describes what information we process to ensure that
       Incosplay, Timegram and other products and features offered by
-      Test1 Inc. ("Incosplay Products" or "Products"). For more
+      Whooli Inc. ("Incosplay Products" or "Products"). For more
       tools and information, see the Incosplay Settings and Timegram
       Settings. The Privacy Policy defines the Incosplay User Information
       Protection Policy.
@@ -536,7 +536,7 @@
         Contact 
          email:
         <a href="mailto:support@incosplay.top?subject=Policy%20question"
-          >Test1098772@gmail.com</a
+          >Whooli098772@gmail.com</a
         >
       </address>
     </section>
