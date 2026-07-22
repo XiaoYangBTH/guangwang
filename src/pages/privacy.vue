cd7b5ce4bc84d6f6cb52729fccfe367eb6f40cbd
XiaoYangBTH <1351072690@qq.com>
2026-07-22 11:16:23 +0800

    1

--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="pp-container">
     <header class="pp-header">
-      <h1>Rovco Privacy Policy</h1>
+      <h1>Kiara Privacy Policy</h1>
       <p class="pp-updated">Date of the latest edition: July 14, 2026</p>
     </header>
 
     <p class="pp-intro">
       This policy describes what information we process to ensure that
       Incosplay, Timegram and other products and features offered by
-      Rovco Inc. ("Incosplay Products" or "Products"). For more
+      Kiara Inc. ("Incosplay Products" or "Products"). For more
       tools and information, see the Incosplay Settings and Timegram
       Settings. The Privacy Policy defines the Incosplay User Information
       Protection Policy.
@@ -536,7 +536,7 @@
         Contact 
          email:
         <a href="mailto:support@incosplay.top?subject=Policy%20question"
-          >Rovco098772@gmail.com</a
+          >Kiara098772@gmail.com</a
         >
       </address>
     </section>
